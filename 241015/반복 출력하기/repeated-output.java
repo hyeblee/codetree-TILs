6f7f8d740b6e2f04eff5f6ef9adaaada3221726b
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-10-15 09:56:43 +0900

    Title: 반복 출력하기, Time: 187ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241015/반복 출력하기/repeated-output.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void printLine(int N){
+        for(int i=0;i<N;i++){
+            System.out.println("12345^&*()_");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        printLine(N);
+    }
+}
