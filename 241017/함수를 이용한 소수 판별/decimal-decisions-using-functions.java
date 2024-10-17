dc3acd47b069aeeb6f59a583b77f7bbfb18b7ad3
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-10-17 17:57:54 +0900

    Title: 함수를 이용한 소수 판별, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241017/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static boolean isPrime(int n){
+        if(n==1)
+            return false;
+        for(int i=2;i<n;i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum=0;
+        for(int i=a;i<=b;i++){
+            if(isPrime(i)){
+                sum+=i;
+            }
+        }
+        System.out.println(i);
+    }
+}
