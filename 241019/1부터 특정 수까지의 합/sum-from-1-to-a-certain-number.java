85812a0e99b85c620c71432822f7fdd42b29a0c9
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-10-19 22:52:57 +0900

    Title: 1부터 특정 수까지의 합, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241019/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int sumN(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++)
+            sum += i;
+
+        return sum / 10;
+    }
+
+    public static void main(String[] args) {
+        // 변수 선언 및 입력:
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(sumN(n));
+    }
+}
