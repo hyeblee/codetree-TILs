7db6a493ef87d94b70c7b7be683bc252ded8cf1d
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-10-19 22:41:41 +0900

    Title: 최대공약수 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241019/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {    
+    // n과 m의 최대공약수를 출력합니다.
+    public static void findGcd(int n, int m) {
+        int gcd = 0;
+        for(int i = 1; i <= Math.min(n, m); i++) {
+            if(n % i == 0 && m % i == 0)
+                gcd = i;
+        }
+    
+        System.out.print(gcd);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        findGcd(n, m);
+    }
+}
