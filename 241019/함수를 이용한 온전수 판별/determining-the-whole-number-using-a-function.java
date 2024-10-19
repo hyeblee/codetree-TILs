5abe59f1910c9d340744b77ab37c316f584b1ec4
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-10-19 23:29:41 +0900

    Title: 함수를 이용한 온전수 판별, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241019/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isOnjeonsu(int n){
+        if(n%2==0)
+            return false;
+        if(n%10==5)
+            return false;
+        if(n%3==0&&n%9!=0)
+            return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+
+        for(int i=a;i<=b;i++){
+            if(isOnjeonsu(i))
+                cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
