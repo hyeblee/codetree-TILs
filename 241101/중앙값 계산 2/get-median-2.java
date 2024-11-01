100a5f8e7d652d8f9304666fbe5475185b49f150
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-01 17:45:45 +0900

    Title: 중앙값 계산 2, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 241101/중앙값 계산 2/get-median-2.java
+++ 241101/중앙값 계산 2/get-median-2.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
+    public static final int MAX_N = 100;
+
+    public static int[] arr = new int[MAX_N];
+    public static int n;
+
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
+        n = sc.nextInt();
         for(int i=0;i<n;i++){
-            int number = sc.nextInt();
-            list.add(number);
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
             if(i%2==0){
-                Collections.sort(list);
-                System.out.print(list.get(list.size()/2)+" ");
+                Arrays.sort(arr,0,i+1);
+                System.out.print(arr[(i+1)/2]+" ");
             }
         }
     }
