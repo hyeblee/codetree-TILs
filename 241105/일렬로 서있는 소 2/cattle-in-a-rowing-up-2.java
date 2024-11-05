7b0ea39aa0c74034dfcc5153a2ea09ae3bb02232
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-05 23:48:49 +0900

    Title: 일렬로 서있는 소 2, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241105/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static int n;
+	public static int[] arr = new int[100];
+
+	public static void main(String[] args) {
+		int cnt = 0;
+
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				for (int k = j + 1; k < n; k++) {
+					if(arr[i]<=arr[j]&&arr[j]<arr[k]) {
+						cnt++;
+					}
+				}
+			}
+		}
+		System.out.println(cnt);
+
+	}
+}
