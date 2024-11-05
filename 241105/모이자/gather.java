aca32378ed420e5045a1588076f0232212e58024
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-05 23:30:07 +0900

    Title: 모이자, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 241105/모이자/gather.java
+++ 241105/모이자/gather.java
@@ -2,29 +2,23 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static int n;
-    public static int[] arr;
+	public static int n;
+	public static int[] arr = new int[100];
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-    	Scanner sc = new Scanner(System.in);
-    	n = sc.nextInt();
-    	int minSumMove = Integer.MAX_VALUE;
-    	arr = new int[n];
-    	for(int i=0;i<n;i++) {
-    		arr[i] = sc.nextInt();
-    	}
+    	int minSum = Integer.MAX_VALUE;
     	
-    	
-    	for(int i=0;i<n;i++) {
-    		int sumMove = 0;
-    		for(int j=0;j<n;j++) {
-    			sumMove += Math.abs(i-j)*arr[j];
-    		}
-    		if(sumMove<minSumMove) {
-    			minSumMove = sumMove;
-    		}
-    	}
-    	System.out.println(minSumMove);
-        
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for(int i=0;i<n;i++) {
+        	arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++) {
+        	int sumDistance = 0;
+        	for(int j=0;j<n;j++) {
+        		sumDistance += Math.abs(i-j)*arr[j];
+        	}
+        	minSum = Math.min(sumDistance, minSum);
+        }
+        System.out.println(minSum);
     }
 }
