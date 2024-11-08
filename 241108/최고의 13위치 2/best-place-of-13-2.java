74cb4a52d1518c60d4d7731f73d8357ce1a0b43e
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-08 12:04:32 +0900

    Title: 최고의 13위치 2, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 241108/최고의 13위치 2/best-place-of-13-2.java
+++ 241108/최고의 13위치 2/best-place-of-13-2.java
@@ -13,16 +13,24 @@ public class Main {
 			}
 		}
 		int max = 0;
+		// 한 줄에 2개 가능하다면?
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n - 2; j++) {
-				for (int k = i+1; k < n; k++) {
-					for (int l = 0; l < n - 2; l++) {
+				for (int k = i + 1; k < n; k++) {
+					for (int l = j; l < n - 2; l++) {
 						int cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[k][l] + arr[k][l + 1] + arr[k][l + 2];
 						max = cnt > max ? cnt : max;
 					}
 				}
+				// 한 줄에 두 칸 다 있는 경우
+				for (int l = j + 3; l < n - 2; l++) {
+					int cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i][l] + arr[i][l + 1] + arr[i][l + 2];
+					max = cnt > max ? cnt : max;
+				}
+
 			}
 		}
+
 		System.out.println(max);
 
 	}
