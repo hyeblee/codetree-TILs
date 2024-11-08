eb7532df1cbc6e79a111c3a4e6927a99d0840470
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-08 16:12:35 +0900

    Title: G or H 3, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241108/G or H 3/G-or-H-3.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		// 여기에 코드를 작성해주세요.
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int[] arr = new int[10001];
+		for(int i=0;i<n;i++) {
+			int position = sc.nextInt();
+			char alphabet = sc.next().charAt(0);
+			arr[position] += alphabet - 'F';
+		}
+		int maxSum=0;
+		for(int i=0;i<=n-k+1;i++) {
+			int sum = 0;
+			for(int j=1;j<=k+1;j++) {
+				sum+=arr[j+i];
+			}
+			maxSum = Math.max(maxSum, sum);
+		}
+		System.out.println(maxSum);
+		
+	}
+}
