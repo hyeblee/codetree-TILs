da7308cf4bd5962be4a3ae759e6ac46fb5c11b99
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-08 11:54:17 +0900

    Title: 최고의 13위치 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241108/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		// 여기에 코드를 작성해주세요.
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		int max = 0;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = 0; j < n - 2; j++) {
+				for (int k = i+1; k < n; k++) {
+					for (int l = 0; l < n - 2; l++) {
+						int cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[k][l] + arr[k][l + 1] + arr[k][l + 2];
+						max = cnt > max ? cnt : max;
+					}
+				}
+			}
+		}
+		System.out.println(max);
+
+	}
+}
