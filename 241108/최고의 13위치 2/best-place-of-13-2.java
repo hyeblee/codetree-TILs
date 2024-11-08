52a5e9a89e007fb8d14a87edef2c3e604cd10ffb
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-08 12:16:32 +0900

    Title: 최고의 13위치 2, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241108/최고의 13위치 2/best-place-of-13-2.java
+++ 241108/최고의 13위치 2/best-place-of-13-2.java
@@ -13,19 +13,16 @@ public class Main {
 			}
 		}
 		int max = 0;
-		// 한 줄에 2개 가능하다면?
+		// 한 줄에 2개 가능하다면? -> 걍 모든 경우의 수 탐색하는데 아닌 걸 어떻게 거를지 생각해야해
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n - 2; j++) {
-				for (int k = i + 1; k < n; k++) {
-					for (int l = j; l < n - 2; l++) {
+				for (int k = i; k < n; k++) {
+					for (int l = 0; l < n - 2; l++) {
+						if(i==k&&Math.abs(l-j)<3)
+							continue;
 						int cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[k][l] + arr[k][l + 1] + arr[k][l + 2];
 						max = cnt > max ? cnt : max;
 					}
-				}
-				// 한 줄에 두 칸 다 있는 경우
-				for (int l = j + 3; l < n - 2; l++) {
-					int cnt = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i][l] + arr[i][l + 1] + arr[i][l + 2];
-					max = cnt > max ? cnt : max;
 				}
 
 			}
