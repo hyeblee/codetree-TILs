f52acaa9fcbc6b212964f5d2680f9d21e6907d0f
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-16 19:15:13 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 647ms, Memory: 23MB, Status: Passed - CodeTree

--- 241116/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.java
+++ 241116/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.java
@@ -1,33 +1,33 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class Main {
-	public static final int MAX_N = 100000;
+class Jewelry implements Comparable<Jewelry> {
+	int weight;
+	int value;
 
-	public static int n, m;
-
-	public static class Jewelry implements Comparable<Jewelry> {
-		int weight;
-		int value;
-
-		public Jewelry(int weight, int value) {
-			this.weight = weight;
-			this.value = value;
-		}
-		
-		public double valuePerWeight() {
-			return (double)value/weight;
-		}
-		// (int)(v1-v2)가 안되는 이유?
-		// -0.0031도 0으로 처리하기 때문!
-		
-		@Override
-		public int compareTo(Jewelry o) {
-			double v1 = this.valuePerWeight();
-			double v2 = o.valuePerWeight();
-			return Double.compare(v2, v1);
-		}
+	public Jewelry(int weight, int value) {
+		this.weight = weight;
+		this.value = value;
 	}
+
+	public double valuePerWeight() {
+		return (double) value / weight;
+	}
+
+	// (int)(v1-v2)가 안되는 이유?
+	// -0.0031도 0으로 처리하기 때문!
+	@Override
+	public int compareTo(Jewelry o) {
+		double v1 = this.valuePerWeight();
+		double v2 = o.valuePerWeight();
+		return Double.compare(v2, v1);
+	}
+}
+
+public class Main {
+	public static int n, m;
+	public static ArrayList<Jewelry> jewels = new ArrayList<>();
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -38,22 +38,24 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			int w = sc.nextInt();
 			int v = sc.nextInt();
-			arr[i] = new Jewelry(w,v);
+			Jewelry jewelry = new Jewelry(w,v);
+			jewels.add(jewelry);
 		}
 
-		Arrays.sort(arr);
+		Collections.sort(jewels);
 		double benefit = 0;
-		int idx = 0;
-		for(int i=0;i<n;i++) {
-			if(arr[idx].weight>m) {
-				benefit += m*arr[idx].valuePerWeight();
+		for (int i = 0; i < n; i++) {
+			int w = jewels.get(i).weight;
+			int v = jewels.get(i).value;
+			
+			if(w>m) {
+				benefit += jewels.get(i).valuePerWeight() * m;
 				break;
 			}
-			benefit += arr[idx].valuePerWeight()*arr[idx].weight;
-			m -= arr[idx].weight;
-			idx++;
+			m-=w;
+			benefit += jewels.get(i).valuePerWeight()*w;
 		}
-		System.out.printf("%.3f\n",benefit);
+		System.out.printf("%.3f\n", benefit);
 	}
 
 }
