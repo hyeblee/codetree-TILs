65104afb64aff3cdf7c171d3382a15f9c3508b05
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-16 21:57:00 +0900

    Title: 회의실 준비 구현, Time: 1067ms, Memory: 52MB, Status: Wrong Answer - CodeTree

---
+++ 241116/회의실 준비 구현/implement-scheduling-meeting-room.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+class Meeting implements Comparable<Meeting>{
+	int s;
+	int e;
+	
+	public Meeting(int s, int e) {
+		this.s = s;
+		this.e = e;
+	}
+	
+	@Override
+	public int compareTo(Meeting o) {
+		return Integer.compare(this.e, o.e);
+	}
+	
+}
+
+public class Main {
+	public static final int MAX_N = 100000;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Meeting> meetings = new ArrayList<>();
+		int n = sc.nextInt();
+		for(int i=0;i<n;i++) {
+			int s = sc.nextInt();
+			int e = sc.nextInt();
+			
+			Meeting metting = new Meeting(s,e);
+			meetings.add(metting);
+		}
+		
+		Collections.sort(meetings);
+		
+		int answer = 0;
+		int current = 0;
+		for(int i=0;i<meetings.size();i++) {
+			int s = meetings.get(i).s;
+			if(s>=current)
+				answer++;
+		}
+		answer--;
+		System.out.println(answer);
+		
+	}
+
+}
