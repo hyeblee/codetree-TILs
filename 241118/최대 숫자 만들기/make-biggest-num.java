31eb6ff1d314e219148ffee77f13fb4c8b09cd02
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-11-18 23:09:53 +0900

    Title: 최대 숫자 만들기, Time: 131ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241118/최대 숫자 만들기/make-biggest-num.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
+
+public class Main {
+	public static final int MAX_N = 50000;
+	
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		String[] arr = new String[n];
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.next();
+		}
+		Arrays.sort(arr, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				if(o2.charAt(0)!=o1.charAt(0))
+					return Integer.compare(o2.charAt(0), o1.charAt(0));
+				if(o1.startsWith(o2)) {
+					System.out.println(o1+":"+o2);
+					int length = o2.length();
+					return Integer.compare(o2.charAt(0),o1.charAt(length));
+				}
+				else {
+					int length = o1.length();
+					return Integer.compare(o2.charAt(length),o1.charAt(0));
+				}
+			}
+			
+		});
+		String answer ="";
+		for(String str:arr) {
+			answer+=str;
+		}
+		System.out.println(answer);
+	}
+
+}
