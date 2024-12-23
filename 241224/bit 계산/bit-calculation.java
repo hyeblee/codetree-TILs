15c88a8b349fa7fc8918c6553132387488886706
hyeblee <124981825+hyeblee@users.noreply.github.com>
2024-12-24 00:48:36 +0900

    Title: bit 계산, Time: 97ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241224/bit 계산/bit-calculation.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int M;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder("");
+		M = Integer.parseInt(br.readLine());
+		int s = 0;
+		for(int i=0;i<M;i++) {
+			String input = br.readLine();
+			StringTokenizer st = new StringTokenizer(input," ");
+			String cmd = st.nextToken();
+			if(cmd.equals("clear")) {
+				s = 0;
+			} else if (cmd.equals("add")) {
+				int x = Integer.parseInt(st.nextToken());
+				if(((s>>x)&1)!=0) {
+					return;
+				}
+				s += 1<<x;
+			}
+			else if (cmd.equals("delete")) {
+				int x = Integer.parseInt(st.nextToken());
+				s -= (1<<x);
+			}
+			else if (cmd.equals("print")) {
+				int x = Integer.parseInt(st.nextToken());
+				if(((s>>x)&1)==0)
+					System.out.println("0");
+				else
+					System.out.println("1");
+			}
+			else if (cmd.equals("toggle")) {
+				int x = Integer.parseInt(st.nextToken());
+				s^=(1<<x);
+			}
+		}
+	}
+}
