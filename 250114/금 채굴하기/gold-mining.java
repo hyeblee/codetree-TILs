8f5e075e616cc8fc1d2b208d4b3b7e6f68792a0c
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-01-14 15:37:21 +0900

    Title: 금 채굴하기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250114/금 채굴하기/gold-mining.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static int n, m;
+	public static int[][] grid = new int[20][20];
+
+	public static int sum(int y, int x, int k) {
+		int sum = 0;
+		
+		for (int dy = y - k; dy <= y + k; dy++) {
+			for (int dx = x - k; dx <= x + k; dx++) {
+				if(dy<0||dy>=n||dx<0||dx>=n)
+					continue;
+				sum += grid[dy][dx];
+			}
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				grid[i][j] = sc.nextInt();
+			}
+		}
+		int max = 0;
+		int maxCnt = 0;
+		for (int k = 1; k <= n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					int num = sum(i,j,k);
+					if(num*m-k*k-(k+1)*(k+1)>max) {
+						max = num*m-k*k-(k+1)*(k+1);
+						maxCnt = num;
+					}
+				}
+			}
+		}
+		System.out.println(maxCnt);
+	}
+}
