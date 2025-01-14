5b9e8906791f1729980af47ec44153202a2ebf08
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-01-14 16:11:09 +0900

    Title: 금 채굴하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 250114/금 채굴하기/gold-mining.java
+++ 250114/금 채굴하기/gold-mining.java
@@ -35,13 +35,13 @@ public class Main {
 			}
 		}
 		int maxCnt = 0;
-		for (int k = 0; k <= 3*n; k++) {
+		for (int k = 0; k <= 2*(n-1); k++) {
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
 					int num = sum(i,j,k);
 					if(num*m>=k*k+(k+1)*(k+1)) {
 //						System.out.println(k*k+(k+1)*(k+1)+":  "+num);
-						maxCnt = num;
+						maxCnt = Math.max(maxCnt,num);
 					}
 				}
 			}
