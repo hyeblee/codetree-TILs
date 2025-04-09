d487afb9673da2da1909fc2ad883f20d159d425d
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-09 14:23:46 +0900

    Title: 그 요일은, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250409/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static int[] endOfMonth = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  public static String[] dayOfWeek = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+  public static int getDay(int m, int d) {
+    int elapsedDays = 0;
+    for (int i = 1; i < m; i++) {
+      elapsedDays += endOfMonth[i];
+    }
+    elapsedDays += d;
+    return elapsedDays;
+  }
+
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int m1 = sc.nextInt();
+    int d1 = sc.nextInt();
+    int m2 = sc.nextInt();
+    int d2 = sc.nextInt();
+    String A = sc.next();
+
+    int n = 0;
+    for (int i = 0; i < dayOfWeek.length; i++) {
+      if (A.equals(dayOfWeek[i])) {
+        n = i;
+        break;
+      }
+    }
+    d1 += n;
+    if (d1 > endOfMonth[m1]) {
+      d1 -= endOfMonth[m1];
+      m1++;
+    }
+    int diff = getDay(m2, d2) - getDay(m1, d1);
+    System.out.println(diff / 7 + 1);
+
+  }
+}
