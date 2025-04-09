ed6230da361777d314dd92bd399ebca1e0c8cf96
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-09 12:25:56 +0900

    Title: DateTime to DateTime, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250409/DateTime to DateTime/datetime-to-datetime.java
+++ 250409/DateTime to DateTime/datetime-to-datetime.java
@@ -3,24 +3,23 @@ import java.util.*;
 
 public class Main {
 
-  public static int getMinute(int d, int h, int m) {
-    int day = d - 11;
-    int minute = h * 60 + m;
-    minute -= 11 * 60 + 11;
-    if (minute < 0) {
-      day--;
-      minute += 24 * 60;
-    }
-    minute += day * 24 * 60;
-    return minute;
+  public static int getMinute(int h, int m) {
+    return h * 60 + m;
   }
+
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int A = sc.nextInt();
     int B = sc.nextInt();
     int C = sc.nextInt();
-
-    System.out.println(getMinute(A, B, C));
+    A -= 11;
+    int minute = getMinute(B, C) - getMinute(11, 11);
+    if (minute < 0) {
+      minute += 24 * 60;
+      A--;
+    }
+    minute += A * 24 * 60;
+    System.out.println(minute);
   }
 }
