15fa3b7c0dc179f4cea25b50041e2c1e1ca7dd3b
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-09 12:08:19 +0900

    Title: Date to Date, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250409/Date to Date/date-to-date.java
+++ 250409/Date to Date/date-to-date.java
@@ -3,24 +3,16 @@ import java.util.*;
 
 public class Main {
 
-  //  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12
+  //                                    1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12
   public static int[] endOfMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
   public static int getDay(int m, int d) {
     int elapsedDays = 0;
-    int month = 1;
-    int day = 1;
-    while (true) {
-      if (month == m && day == d) {
-        return elapsedDays;
-      }
-      day++;
-      elapsedDays++;
-      if (day > endOfMonth[month]) {
-        month++;
-        day = 1;
-      }
+    for (int i = 0; i < m; i++) {
+      elapsedDays += endOfMonth[i];
     }
+    elapsedDays += d;
+    return elapsedDays;
   }
 
   public static void main(String[] args) {
@@ -30,6 +22,6 @@ public class Main {
     int m2 = sc.nextInt();
     int d2 = sc.nextInt();
 
-    System.out.println(getDay(m2, d2) - getDay(m1, d1));
+    System.out.println(getDay(m2, d2) - getDay(m1, d1) + 1);
   }
 }
