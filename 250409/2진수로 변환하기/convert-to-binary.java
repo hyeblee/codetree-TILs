dd21537a61b9204dffaa21a1084727cd1b67fe3d
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-09 14:59:26 +0900

    Title: 2진수로 변환하기, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250409/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static int[] binary = new int[20];
+  public static int length = 0;
+
+  public static void decimalToBinary(int decimal) {
+    while(decimal > 0) {
+      binary[length++] = decimal % 2;
+      decimal /= 2;
+    }
+  }
+
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    decimalToBinary(n);
+
+    for(int i = length - 1; i >= 0; i--) {
+      System.out.print(binary[i]);
+    }
+
+  }
+}
