9b77ee25895626556a183ab9c76778d89b5a7598
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-14 10:19:28 +0900

    Title: 진수 to 진수, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250414/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static int A, B;
+  public static String N;
+
+  public static int AToDecimal(String N) {
+    int decimal = 0;
+    for(int i = 0; i < N.length(); i++) {
+      decimal = decimal*A + (N.charAt(i) - '0');
+    }
+    return decimal;
+  }
+
+  public static String decimalToB(int decimal) {
+    StringBuilder sb = new StringBuilder();
+
+    while(true){
+      sb.append(decimal%B);
+      decimal = decimal/B;
+      if(decimal==0) break;
+    }
+    return sb.toString();
+  }
+
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    A = sc.nextInt();
+    B = sc.nextInt();
+    N = sc.next();
+    // Please write your code here.
+
+    int decimal = AToDecimal(N);
+    System.out.println(decimalToB(decimal));
+
+  }
+}
