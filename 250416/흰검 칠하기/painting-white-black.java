3b581e4f50535acd7e6d4baf8e8cc8dc3e4d8d49
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-16 16:17:51 +0900

    Title: 흰검 칠하기, Time: 239ms, Memory: 20MB, Status: Passed - Codetree

---
+++ 250416/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+
+public class Main {
+
+  public static final int MAX_SIZE = 100000;
+
+  public static Tile[] tiles = new Tile[MAX_SIZE * 2 + 1];
+  public static int[] result = new int[MAX_SIZE * 2 + 1]; // 1이면 흑, 2이면 백
+  public static int N;
+
+
+  public static class Tile {
+
+    int w;
+    int b;
+  }
+
+  public static void paintBlack(int start, int end) {
+    for (int i = start; i <= end; i++) {
+      tiles[i].b++;
+      result[i] = 1;
+    }
+  }
+
+  public static void paintWhite(int start, int end) {
+    for (int i = start; i <= end; i++) {
+      tiles[i].w++;
+      result[i] = 2;
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+
+    for (int i = 0; i <= MAX_SIZE * 2; i++) {
+      tiles[i] = new Tile();
+    }
+
+    int cur = MAX_SIZE;
+    for (int i = 0; i < N; i++) {
+      int x = sc.nextInt();
+      char ch = sc.next().charAt(0);
+      if (ch == 'L') {
+        paintWhite(cur - x + 1, cur);
+        cur -= x - 1;
+      } else if (ch == 'R') {
+        paintBlack(cur, cur + x - 1);
+        cur += x - 1;
+      }
+    }
+    int w = 0;
+    int b = 0;
+    int g = 0;
+
+    for (int i = 0; i <= MAX_SIZE * 2; i++) {
+      if (tiles[i].b >= 2 && tiles[i].w >= 2) {
+        g++;
+      } else if (result[i] == 1) {
+        b++;
+      } else if (result[i] == 2) {
+        w++;
+      }
+
+    }
+
+    System.out.println(w + " " + b + " " + g);
+  }
+
+}
