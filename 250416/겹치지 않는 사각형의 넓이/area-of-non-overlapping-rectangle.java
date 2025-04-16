bc174dab3f7c0ed05d58a3c9b01c68eaa12be417
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-16 17:33:33 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 192ms, Memory: 26MB, Status: Passed - Codetree

---
+++ 250416/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+  public static final int MAX_SIZE = 1000;
+  public static int[][] grid = new int[MAX_SIZE * 2 + 1][MAX_SIZE * 2 + 1];
+
+  public static void makeRectangle(int x1, int y1, int x2, int y2) {
+    for (int i = y1; i < y2; i++) {
+      for (int j = x1; j < x2; j++) {
+        grid[i][j] = 1;
+      }
+    }
+  }
+
+  public static void deleteRectangle(int x1, int y1, int x2, int y2) {
+    for (int i = y1; i < y2; i++) {
+      for (int j = x1; j < x2; j++) {
+        grid[i][j] = 0;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    for (int i = 0; i < 2; i++) {
+      int x1 = sc.nextInt();
+      int y1 = sc.nextInt();
+      int x2 = sc.nextInt();
+      int y2 = sc.nextInt();
+      makeRectangle(x1 + MAX_SIZE, y1 + MAX_SIZE, x2 + MAX_SIZE, y2 + MAX_SIZE);
+    }
+
+    int x1 = sc.nextInt();
+    int y1 = sc.nextInt();
+    int x2 = sc.nextInt();
+    int y2 = sc.nextInt();
+    deleteRectangle(x1 + MAX_SIZE, y1 + MAX_SIZE, x2 + MAX_SIZE, y2 + MAX_SIZE);
+
+    int result = 0;
+    for (int i = 0; i <= MAX_SIZE * 2; i++) {
+      for (int j = 0; j <= MAX_SIZE * 2; j++) {
+        if (grid[i][j] == 1) {
+          result++;
+        }
+      }
+    }
+
+    System.out.println(result);
+  }
+}
