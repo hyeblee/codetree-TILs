123dfc4ef4308eed90e0606ef0634be85a8cef28
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-04-17 16:12:05 +0900

    Title: 색종이의 총 넓이, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250417/색종이의 총 넓이/the-total-area-of-colored-paper.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+
+  public static final int MAX_SIZE = 100;
+  public static int[][] grid = new int[MAX_SIZE * 2 + 1][MAX_SIZE * 2 + 1];
+
+  public static void makeRectangle(int x, int y) {
+    for(int i=y;i<y+8;i++){
+      for(int j=x;j<x+8;j++){
+        grid[i][j] = 1;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] x = new int[n];
+    int[] y = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      x[i] = sc.nextInt();
+      y[i] = sc.nextInt();
+      makeRectangle(x[i]+MAX_SIZE, y[i]+MAX_SIZE);
+    }
+    int count = 0;
+    for (int i = 0; i < 2*MAX_SIZE+1; i++) {
+      for (int j = 0; j < 2 * MAX_SIZE + 1; j++) {
+        if(grid[i][j] == 1){
+          count++;
+        }
+      }
+    }
+
+    System.out.println(count);
+  }
+}
