c7b93ae157e6877d68f33b2aea32a50a88371c8f
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-05-08 09:15:14 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250508/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Main {
+  public static final int MAX_SIZE = 100;
+  public static int[][] grid = new int[MAX_SIZE*2+1][MAX_SIZE*2+1];
+
+  public static void makeRectangle(int x1, int y1, int x2, int y2) {
+    for (int i = y1; i <= y2; i++) {
+      for(int j = x1; j <= x2; j++) {
+        grid[i][j] = 1;
+      }
+    }
+  }
+
+  public static void eraseRectangle(int x1, int y1, int x2, int y2) {
+    for (int i = y1; i <= y2; i++) {
+      for(int j = x1; j <= x2; j++) {
+        grid[i][j] = 0;
+      }
+    }
+  }
+
+ public static int getMinSize(int x1, int y1, int x2, int y2) {
+    int count = 0;
+    for (int i = y1; i <= y2; i++) {
+        for (int j = x1; j <= x2; j++) {
+            if (grid[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int rect1_x1 = sc.nextInt()+MAX_SIZE;
+    int rect1_y1 = sc.nextInt()+MAX_SIZE;
+    int rect1_x2 = sc.nextInt()+MAX_SIZE;
+    int rect1_y2 = sc.nextInt()+MAX_SIZE;
+    int rect2_x1 = sc.nextInt()+MAX_SIZE;
+    int rect2_y1 = sc.nextInt()+MAX_SIZE;
+    int rect2_x2 = sc.nextInt()+MAX_SIZE;
+    int rect2_y2 = sc.nextInt()+MAX_SIZE;
+    if(rect2_x1<=rect1_x1&&rect1_x2<=rect2_x2&&rect2_y1<=rect1_y1&&rect1_y2<=rect2_y2) {
+      System.out.println("0");
+      return;
+    }
+    makeRectangle(rect1_x1, rect1_y1, rect1_x2, rect1_y2);
+    eraseRectangle(rect2_x1, rect2_y1, rect2_x2, rect2_y2);
+    int result = getMinSize(rect1_x1, rect1_y1, rect1_x2, rect1_y2);
+    System.out.println(result);
+  }
+}
