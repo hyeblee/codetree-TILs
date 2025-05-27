891a8b93a1c1803cc1c1ab4eb4ea7bfdd2915e2d
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-05-27 23:55:45 +0900

    Title: 만나는 그 순간, Time: 234ms, Memory: 28MB, Status: Passed - Codetree

--- 250527/만나는 그 순간/the-moment-we-meet.java
+++ 250527/만나는 그 순간/the-moment-we-meet.java
@@ -1,76 +1,59 @@
 import java.util.Scanner;
-import java.util.*;
+
 public class Main {
-    public static final int MAX_SIZE = 1000 * 1000;
+    public static final int MAX_T = 1000000;
 
-    // 매 초에 어느 위치에 있었는지 기록하면 됨
-    public static int[] a = new int[MAX_SIZE+1];
-    public static int[] b = new int[MAX_SIZE+1];
-    public static int timeA = 0; // 현재 A의 시간
-    public static int timeB = 0; // 현재 B의 시간
-    public static int curA = 0;
-    public static int curB = 0; // 현재 B의 위치
-
-    // dir, t 
-    // 왼쪽으로 t초만큼 이동
-    // 현재 위치, 현재 초 필요
-
-    public static void goLeft(int[] arr, int curP, int curT, int t) {
-        for(int i=0;i<t;i++){ 
-            arr[curT+i+1] = --curP;
-        }
-    }
-
-    public static void goRight(int[] arr, int curP, int curT, int t) {
-        for(int i=0;i<t;i++){ 
-            arr[curT+i+1] = ++curP;
-        }
-    }
-
+    public static int n, m;
+    public static int[] posA = new int[MAX_T + 1];
+    public static int[] posB = new int[MAX_T + 1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int maxT = 0;
-        for(int i=0;i<n;i++){
-            char dir = sc.next().charAt(0);
+        
+        int timeA = 1;
+        for(int i=0;i<n;i++) {
+            char d = sc.next().charAt(0);
             int t = sc.nextInt();
-            maxT += t;
-            if(dir=='L'){
-                goLeft(a, curA, timeA, t);
-                curA -= t;
-                timeA += t;
-            } else {
-                goRight(a, curA, timeA, t);
-                curA += t;
-                timeA += t;
+
+            while(t > 0) {
+                if(d == 'R')
+                    posA[timeA] = posA[timeA - 1] + 1;
+                else
+                    posA[timeA] = posA[timeA - 1] - 1;
+                timeA++;
+                t--;
             }
-            // System.out.println("A: "+curA);
+        }
+        
+        int timeB = 1;
+        for(int i = 0; i < m; i++) {
+            char d = sc.next().charAt(0); 
+            int t = sc.nextInt();
+            
+            while(t > 0) {
+                if(d == 'R')
+                    posB[timeB] = posB[timeB - 1] + 1;
+                else
+                    posB[timeB] = posB[timeB - 1] - 1;
+                
+                timeB++;
+                t--;
+            }
         }
 
-         for(int i=0;i<m;i++){
-            char dir = sc.next().charAt(0);
-            int t = sc.nextInt();
-            if(dir=='L'){
-                goLeft(b, curB, timeB, t);
-                curB -= t;
-                timeB += t;
-            } else {
-                goRight(b, curB, timeB, t);
-                curB += t;
-                timeB += t;
-            }
-            // System.out.println(curB);
-        }
-        // System.out.println(Arrays.toString(a));
-        // System.out.println(Arrays.toString(b));
-        for(int i=1;i<=maxT;i++){
-            if(a[i]==b[i]){
-                System.out.println(i);
-                return;
+        int result = -1;
+        for(int i=1;i < timeA; i++) {
+            if(posA[i] == posB[i]) {
+                result = i;
+                break;
             }
         }
-        System.out.println(-1);
+
+        System.out.println(result);
+
+        
+        // Please write your code here.
     }
 }
