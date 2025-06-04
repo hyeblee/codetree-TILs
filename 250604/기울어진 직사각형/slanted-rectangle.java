c2c53a5f52c479fd7fbbf8d6427c55f98b2f5c26
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-06-04 12:51:19 +0900

    Title: 기울어진 직사각형, Time: 155ms, Memory: 12MB, Status: Passed - Codetree

--- 250604/기울어진 직사각형/slanted-rectangle.java
+++ 250604/기울어진 직사각형/slanted-rectangle.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.*;
+
 public class Main {
 
   public static final int[] dx = {1, -1, -1, 1}; // 1, 2, 3, 4 이동
@@ -10,60 +11,26 @@ public class Main {
   public static int[][] grid;
 
   public static boolean isInRange(int y, int x) {
-    return 0<=y&&y<n && 0<=x&&x<n;
+    return 0 <= y && y < n && 0 <= x && x < n;
   }
 
   public static int getRectangle(int y, int x, int one, int two) {
     int curY = y;
     int curX = x;
     int sum = 0;
+    int[] moveNum = new int[]{one, two, one, two}; // 1,2,3,4 방향 탐색 갯수
 
-    // System.out.printf("%d %d %d\n",curX, curY, sum);
-    for(int i=0;i<one;i++) { // 1방향으로 탐색
-
-    //   System.out.printf("%d %d %d\n",curX, curY, sum);
-      curX += dx[0];
-      curY += dy[0];
-      if (!isInRange(curY, curX)) {
-        // System.out.println("1");
-        return -1;
+    for (int i = 0; i < 4; i++) { // 4방향 전부 탐색
+      for (int j = 0; j < moveNum[i]; j++) { // moveNum갯수만큼 탐색
+        curY += dy[i];
+        curX += dx[i];
+        if (!isInRange(curY, curX)) {
+          return -1;
+        }
+        sum += grid[curY][curX];
       }
-      sum += grid[curY][curX];
-    }
-
-    for(int i=0;i<two;i++) { // 2방향으로 탐색
-    //   System.out.printf("%d %d %d\n",curX, curY, sum);
-      curX += dx[1];
-      curY += dy[1];
-      if (!isInRange(curY, curX)) {
-        // System.out.println("2");
-        return -1;
-      }
-      sum += grid[curY][curX];
-    }
 
 
-    for(int i=0;i<one;i++) { // 3방향으로 탐색
-    //   System.out.printf("%d %d %d\n",curX, curY, sum);
-      curX += dx[2];
-      curY += dy[2];
-      if (!isInRange(curY, curX)) {
-        // System.out.println("3");
-        return -1;
-      }
-      sum += grid[curY][curX];
-    }
-
-
-    for(int i=0;i<two;i++) { // 4방향으로 탐색
-    //   System.out.printf("%d %d %d\n",curX, curY, sum);
-      curX += dx[3];
-      curY += dy[3];
-      if (!isInRange(curY, curX)) {
-        // System.out.println("4");
-        return -1;
-      }
-      sum += grid[curY][curX];
     }
     return sum;
   }
@@ -82,19 +49,16 @@ public class Main {
 
     int max = 0;
 
-//   System.out.println( getRectangle(4,1,3,1));
-     for (int i = 0; i < n; i++) {
-     	for (int j = 0; j< n; j++) { // 시작 위치
-     		for(int k=1; k<=max_size;k++) {
-     			for(int l=1; l<=max_size;l++) {
-     				int result = getRectangle(i,j,k,l);
-     				// System.out.printf("%d %d %d %d = %d\n", j,i, l,k,result);
-
-     				max = Math.max(max, result);
-     			}
-     		}
-     	}
-     }
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) { // 시작 위치
+        for (int k = 1; k <= max_size; k++) {
+          for (int l = 1; l <= max_size; l++) {
+            int result = getRectangle(i, j, k, l);
+            max = Math.max(max, result);
+          }
+        }
+      }
+    }
 
     System.out.println(max);
   }
