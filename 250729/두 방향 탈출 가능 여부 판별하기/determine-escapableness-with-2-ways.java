2ec0c1fe8283ea386323e10e6e6c078628af1dc3
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-07-29 00:00:48 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250729/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class Main {
+    public static int[] dx = {0,1};
+    public static int[] dy = {-1,0};
+    public static int n, m;
+    public static int[][] grid;
+    public static boolean[][] visited;
+    public static boolean isPossible;
+
+    public static boolean isInRange(int y, int x) {
+        return 0<=y&&y<n && 0<=x&&x<m;
+    }
+
+    public static void dfs(int y, int x) {
+        visited[y][x] = true;
+        if (y==n-1 && x==m-1) {
+            // System.out.println(y+", "+x);
+            isPossible = true;
+            return;
+        }
+        if(isPossible)
+            return;
+
+        for(int i=0;i<2;i++){
+            int curY = y+dy[i];
+            int curX = x+dx[i];
+            if(!isPossible && isInRange(curY,curX) && !visited[curY][curX] && grid[curY][curX]==1) {
+                dfs(curY,curX);
+            }
+        }
+    }
+    
+
+
+    public static void main(String[] args) {
+        // Please write your code here
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        grid = new int[n][m];
+        visited = new boolean[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        dfs(0,0);
+
+        if (isPossible) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
