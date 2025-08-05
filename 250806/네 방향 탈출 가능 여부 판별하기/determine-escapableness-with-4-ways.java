15090204e397c8c3741044be56900c5b7524c2ce
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-08-06 00:29:26 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 3065ms, Memory: 176MB, Status: Time Limit Exceed - Codetree

--- 250806/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 250806/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -7,7 +7,6 @@ public class Main {
     
     public static int n, m;
     public static int[][] grid;
-    public static boolean[][] visited;
     public static int canEscape = 0;
 
     public static class Node{
@@ -34,18 +33,16 @@ public class Main {
             if (cur.y==n-1 && cur.x==m-1) {
                 canEscape = 1;
                 return;
-            }
+            }            
 
-            
-
-            visited[cur.y][cur.x] = true;
+            grid[cur.y][cur.x] = -1;
             
             for (int i=0;i<4;i++) {
                 int curY = cur.y+dy[i];
                 int curX = cur.x+dx[i];
                 if (!inRange(curY, curX)|| grid[curY][curX]==0)
                     continue;
-                if (!visited[curY][curX])
+                if (grid[curY][curX]!=-1)
                     queue.offer(new Node(curY, curX));
             }
         }
@@ -57,7 +54,6 @@ public class Main {
         n = sc.nextInt();
         m = sc.nextInt();
         grid = new int[n][m];
-        visited = new boolean[n][m];
 
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
