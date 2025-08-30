9220b70ea6a7f6b2e1db65f886ed781629827656
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-08-30 17:04:21 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250830/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
+++ 250830/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -1,12 +1,68 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
-    public static 
-    public static 
-    public static 
+    public static int n, k;
+    public static List<Integer> result = new ArrayList<>();
+    public static void printResult() {
+        for(Integer n: result) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+
+    public static void chooseN(int depth) {
+        if (depth == n) {
+            printResult();
+            return;
+        }
+        if (depth <= 1) {
+            for(int i=1;i<=k;i++) {
+                result.add(i);
+                chooseN(depth+1);
+                result.remove(result.size()-1);
+            }
+        } else {
+            int cur = result.get(result.size()-1);
+            int before = result.get(result.size()-2);
+            boolean isSame = cur == before;
+            for(int i=1;i<=k;i++) {
+                if (isSame && i==cur) {
+                    continue;
+                } 
+                result.add(i);
+                result.add(i);
+                chooseN(depth+1);
+                result.remove(result.size()-1);
+            \}
+        \}
+
+    \} 
+    // public static 
+    // public static 
+    // public static 
+
+212public class Main {    public static void chooseN(int depth) {
+
+$0
+                chooseN(depth+1);
+                result.remove(result.size()-1);
+            }
+        }
+
+    } 
+    // public static 
+    // public static 
+    // public static 
+    // public static 
+    // public static 
+    // public static 
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int k = sc.nextInt();
-        int n = sc.nextInt();
-        // Please write your code here.
+        k = sc.nextInt();
+        n = sc.nextInt();
+        
+        chooseN(0);
+        
     }
 }
