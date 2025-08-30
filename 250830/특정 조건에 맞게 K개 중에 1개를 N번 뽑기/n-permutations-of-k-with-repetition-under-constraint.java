9228d9f94c6f941075d2d4b32460b3fd2b82a9d5
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-08-30 16:52:32 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250830/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static 
+    public static 
+    public static 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        // Please write your code here.
+    }
+}
