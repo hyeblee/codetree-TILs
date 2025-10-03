ce7b8681c39ac6dbf657d797c5add6d260905aa5
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-10-03 16:58:56 +0900

    Title: 트리의 지름, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251003/트리의 지름/diameter-of-tree.java
+++ 251003/트리의 지름/diameter-of-tree.java
@@ -2,15 +2,23 @@ import java.util.Scanner;
 import java.io.*;
 
 // 트리의 지름 구하기
+// 1에서 출발하여 제일 먼 정점 x를 구한다 (dfs)
+// x에서 출발하여 제일 먼 정점까지의 거리가 트리의 지름 (dfs)
+
 public class Main {
+    public static List<List<Integer>> adjsList;
+    public static int n;
+    public static boolean[] visited;
+     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         for (int i = 0; i < n - 1; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
             int w = sc.nextInt();
         }
-        // Please write your code here.
+
+
     }
 }
