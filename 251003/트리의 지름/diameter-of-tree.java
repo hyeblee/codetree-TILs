92dca6cbd6e27bd6be807dcd25d747539aa64da1
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-10-03 16:56:06 +0900

    Title: 트리의 지름, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251003/트리의 지름/diameter-of-tree.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.*;
+
+// 트리의 지름 구하기
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n - 1; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+        }
+        // Please write your code here.
+    }
+}
