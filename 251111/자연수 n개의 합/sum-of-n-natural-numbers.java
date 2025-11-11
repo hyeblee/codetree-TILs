0133e3968d7b3e42924778aa9fd7748f469375e6
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-11-11 17:19:19 +0900

    Title: 자연수 n개의 합, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 251111/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 251111/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -2,36 +2,29 @@ import java.util.Scanner;
 
 public class Main {
 
-    static final int MAX = 2*(int)10e8;
-    static long[] sum;
+    static final int MAX = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long s = sc.nextLong();
         // Please write your code here.
-        sum = new long[MAX+1];
-        
-        for(int i=1;i<=MAX;i++) {
-            sum[i] = sum[i-1] + i;
-            // System.out.print(sum[i] + " ");
-        }
-        int l = 0;
-        int r = MAX + 1;
+
+        long l = 0;
+        long r = (long)MAX + 1L;
 
         // 가능한 수 중 최댓값
-        int maxN = 0;
 
         while (l < r) {
-            int mid = (l+r) / 2;
-            if (sum[mid] <= s) {
+            long mid = (l+r) / 2;
+            long sum = (1+mid)*mid/2;
+            if (sum <= s) {
                 l = mid + 1;
             } else {
                 r = mid;
             }
         }
 
-        maxN = l-1;
 
-        System.out.println(maxN);
+        System.out.println(l-1);
     }
 }
