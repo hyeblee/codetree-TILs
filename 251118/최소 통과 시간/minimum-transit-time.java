bd96d1375c52e1b3e230f8d15b1315456d92d725
Hyebin Lee <124981825+hyeblee@users.noreply.github.com>
2025-11-18 18:35:45 +0900

    Title: 최소 통과 시간, Time: 500ms, Memory: 24MB, Status: Wrong Answer - Codetree

--- 251118/최소 통과 시간/minimum-transit-time.java
+++ 251118/최소 통과 시간/minimum-transit-time.java
@@ -1,45 +1,49 @@
 import java.util.*;
 
 public class Main {
+    // upper bound: 초과하는 첫 값을 찾는다. (조건 만족 중 제일 큰 값 찾기에 사용)
+    // lower bound: 이상인 첫 값을 찾는다.
     static int n, m;
     static int[] arr;
 
-    // 주어진 시간 안에 n명 모두 통과 가능한지 체크
-    static boolean isPossible(long seconds) {
-        long count = 0;
+    static boolean isPossible(long second) {
+        long cnt = 0;
         for (int i = 0; i < m; i++) {
-            count += seconds / arr[i]; // 각 통로에서 가능한 인원
-            if (count >= n) return true; // 이미 n명 이상이면 가능
+            long possibleCount = second / arr[i];
+            cnt += possibleCount;
+            if (cnt >= n) {
+                return true;
+            }
         }
-        return count >= n;
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); // 사람 수
-        m = sc.nextInt(); // 통로 수
+        n = sc.nextInt();
+        m = sc.nextInt();
 
         arr = new int[m];
+
         for (int i = 0; i < m; i++)
             arr[i] = sc.nextInt();
 
-        Arrays.sort(arr); // 속도 오름차순 정렬
+        Arrays.sort(arr);
 
-        long left = 1;                 // 최소 시간
-        long right = (long) arr[m - 1] * n; // 최대 시간 (가장 느린 통로로 모두 통과)
-        long answer = right;
+        long l = 0;
+        long r = (long) arr[m-1] * n;
 
-        while (left <= right) {
-            long mid = (left + right) / 2;
+        while (l < r) {
+            long mid = (l + r) / 2;
 
             if (isPossible(mid)) {
-                answer = mid;      // 가능한 최소 시간 갱신
-                right = mid - 1;   // 더 줄일 수 있는지 탐색
+                r = mid - 1;
             } else {
-                left = mid + 1;    // 부족하면 시간 늘리기
+                l = mid + 1;
             }
         }
 
-        System.out.println(answer);
+        System.out.println(r+1);
+
     }
 }
